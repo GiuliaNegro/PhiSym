3ffd84e6eeb7f912b5bd2abff22af82582966a9b
GiuliaNegro <giulia.negro@cern.ch>
2015-05-20 16:14:29 +0200

    new eCut

--- EcalCalibAlgos/src/PhiSymmetryCalibration.cc
+++ EcalCalibAlgos/src/PhiSymmetryCalibration.cc
@@ -385,9 +385,10 @@ void PhiSymmetryCalibration::analyze( const edm::Event& event, const edm::EventS
     float et = ite->energy()/cosh(eta);
     float e  = ite->energy();
 
-    int iRing = CalibRing.getRingIndex(hit); 
+    int iRingEEM = CalibRing.getRingIndex(hit) - EcalRingCalibrationTools::N_RING_BARREL; 
+    int iRingEEP = iRingEEM - EcalRingCalibrationTools::N_RING_ENDCAP/2;
 
-   	cout << "ring number: " << iRing << endl;
+   	cout << "EEM ring number: " << iRingEEM << ", EEP ring number: " << iRingEEP << endl;
 
     // if iterating, multiply by the previous correction factor
     if (reiteration_) {
@@ -409,8 +410,8 @@ void PhiSymmetryCalibration::analyze( const edm::Event& event, const edm::EventS
 	{  
 	  //float eta_ring= abs(e_.cellPos_[ring][50].eta())  ;
 	  //eCut_endc = ap_ + eta_ring*b_;
-      if(hit.zside()>0) eCut_endc = 2*nNoise_*(72.92+(3.549*iRing)+(0.2262*iRing*iRing))/1000;
-      if(hit.zside()<0) eCut_endc = 2*nNoise_*(79.29+(4.148*iRing)+(0.2442*iRing*iRing))/1000;
+      if(hit.zside()>0) eCut_endc = 2*nNoise_*(72.92+(3.549*iRingEEP)+(0.2262*iRingEEP*iRingEEP))/1000;
+      if(hit.zside()<0) eCut_endc = 2*nNoise_*(79.29+(4.148*iRingEEM)+(0.2442*iRingEEM*iRingEEM))/1000;
 
 	}
     }
